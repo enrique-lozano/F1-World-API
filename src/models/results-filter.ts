42ca0d389a3e0237078ff964e2268eafcb30724d
enriqueloz88 <lozano.elc@gmail.com>
2023-12-08 16:37:17 +0100

    feat: Add validators on position ranges

--- src/models/results-filter.ts
+++ src/models/results-filter.ts
@@ -16,10 +16,18 @@ export interface SessionEntrantQueryParams extends SessionQueryParams {
 }
 
 export interface ResultsFiltersQueryParams extends SessionEntrantQueryParams {
-  /** Look for the results where the driver achieved a position worse than or equal to this number.  */
+  /** Look for the results where the driver achieved a position worse than or equal to this number.
+   *
+   * @minimum 1 The minimum value of `minPos` should be 1
+   * @isInt The `minPos` param should be an integer
+   */
   minPos?: number;
 
-  /** Look for the results where the driver achieved a position better than or equal to this number.  */
+  /** Look for the results where the driver achieved a position better than or equal to this number.
+   *
+   * @minimum 1 The minimum value of `maxPos` should be 1
+   * @isInt The `maxPos` param should be an integer
+   */
   maxPos?: number;
 
   /** Filter by a specific postion text, that can be `1`, `2`, `3`... or `DNF`, `DNS`... */
